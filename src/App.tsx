b5fa4768591591e13c86e56b2d14ae3964d30760
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-07-01 03:46:55 +0300

    add map for show products list

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import { products } from "./data/products";
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+
+      {/* <Product product={products[0]} />
+      <Product product={products[1]} /> */}
     </div>
   );
   // return React.createElement("h1", {}, "Hello From JS");
